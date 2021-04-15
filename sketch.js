24e0f5d7e31b9497f84b38f9377313dac88826df
elnazmarediya1 <54216751+elnazmarediya1@users.noreply.github.com>
2021-04-15 13:53:35 -0500

    Liking a chat message

--- sketch.js
+++ sketch.js
@@ -6,6 +6,9 @@ function setup() {
 	ccoin = loadImage("carloscoin.png");
 	wboard = loadImage("WhiteBoardEdit.png");
 	coined = loadImage("coined.png");
+	chat = loadImage("chatedit.png");
+	hearted = loadImage("hearted.png");
+	gray = loadImage("grayheart.png");
 	screenRecord = createVideo("screenRecord.mov");
 	screenRecord.hide();
 	cnv.mousePressed(logic);
@@ -18,13 +21,28 @@ function draw() {
 		image(main, 10, 10, 1200, 769);
 		//rect(1180, 10, 35, 30);
 		//rect(790, 735, 35, 30);
-	} else if (scr == 1) {
-		image(ccoin, 10, 10, 1200, 796);
+
+		//rect(600, 735, 35, 30);
+	} 
+	else if (scr == 1) {
+		image(ccoin, 10, 10, 1200, 769);
 		//rect(1135, 75, 35, 30);
-	} else if (scr == 2) {
+	} 
+	else if (scr == 2) {
 		image(wboard, 10, 10, 1200, 769);
-	} else if (scr == 6) {
-		image(coined, 10, 10, 1200, 796);
+	} 
+	else if (scr == 3){
+		image(chat, 10, 10, 1200, 769);
+		//rect(1160, 55, 35, 30);
+	} 
+	else if (scr == 6) {
+		image(coined, 10, 10, 1200, 769);
+	} 
+	else if (scr == 7) {
+		image(hearted, 10, 10, 1200, 769);
+	} 
+	else if (scr == 8) {
+		image(gray, 10, 10, 1200, 769);
 	} 
 }
 
@@ -35,6 +53,9 @@ function logic() {
 	src2y = 75;
 	closex =1180;
 	closey = 10;
+	chatx = 600;
+	heartx = 1160;
+	hearty = 50;
 	if (
 		mouseX > centerX &&
 		mouseX < centerX + 50 &&
@@ -42,21 +63,24 @@ function logic() {
 		mouseY > centerY
 	) {
 		scr = 1;
-	} else if (
+	} 
+	else if (
 		mouseX > src2x &&
 		mouseX < src2x + 50 &&
 		mouseY < src2y + 50 &&
 		mouseY > src2y
 	) {
 		scr = 6;
-	} else if (
+	} 
+	else if (
 		mouseX > closex &&
 		mouseX < closex + 50  &&
 		mouseY < closey + 50 &&
 		mouseY > closey
 	) {
 		scr = 0;
-	} else if (
+	} 
+	else if (
 		mouseX > centerX &&
 		mouseX < centerX + 100 &&
 		mouseY < centerY + 100 &&
@@ -64,6 +88,23 @@ function logic() {
 	) {
 		scr = 2;
 	} 
+	else if (
+		mouseX > chatx &&
+		mouseX < chatx + 50 &&
+		mouseY < centerY + 50 &&
+		mouseY > centerY
+	) {
+		scr = 3; //change to scr = 8 for carlos' view
+	} 
+	else if (
+		mouseX > heartx &&
+		mouseX < heartx + 50 &&
+		mouseY < hearty + 50 &&
+		mouseY > hearty
+	) {
+		scr = 7;
+	} 
+
 }
 
 
