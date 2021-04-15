7fa6f2e503e5c386f5ea630098a8d14b30c95344
elnazmarediya1 <54216751+elnazmarediya1@users.noreply.github.com>
2021-04-15 12:39:27 -0500

    Whiteboard added

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
 
 
 function setup() {
-	cnv = createCanvas(800, 800);
+	cnv = createCanvas(3000, 2000);
 	main = loadImage("Xoom.png");
 	ccoin = loadImage("carloscoin.png");
-	chat = loadImage("chat.png");
-	loc = loadImage("loc.png");
-	story = loadImage("story.png");
+	wboard = loadImage("WhiteBoardEdit.png");
+	coined = loadImage("coined.png");
 	screenRecord = createVideo("screenRecord.mov");
 	screenRecord.hide();
 	cnv.mousePressed(logic);
@@ -16,59 +15,55 @@ function setup() {
 function draw() {
 	background(245);
 	if (scr == 0) {
-		image(main, 10, 10, 750, 500);
-		// rect(490, 475, 40, 40);
+		image(main, 10, 10, 1200, 769);
+		//rect(1180, 10, 35, 30);
+		//rect(790, 735, 35, 30);
 	} else if (scr == 1) {
-		image(ccoin, 10, 10, 750, 500);
+		image(ccoin, 10, 10, 1200, 796);
+		//rect(1135, 75, 35, 30);
 	} else if (scr == 2) {
-		image(loc, 10, 10, 360, 623);
-	} else if (scr == 3) {
-		image(story, 10, 10, 360, 623);
-	} else if (scr == 4) {
-		image(screenRecord, 10, 10);
-		screenRecord.size(360, 623);
-		screenRecord.loop();
-		image(nobg, 10, 10, 360, 623);
-	}
+		image(wboard, 10, 10, 1200, 769);
+	} else if (scr == 6) {
+		image(coined, 10, 10, 1200, 796);
+	} 
 }
 
 function logic() {
-	centerX = 490;
-	centerY = 475;
-	src2x = 700;
-	src2y = 50;
+	centerX = 790;
+	centerY = 735;
+	src2x = 1130;
+	src2y = 75;
+	closex =1180;
+	closey = 10;
 	if (
 		mouseX > centerX &&
-		mouseX < centerX + 40 &&
-		mouseY < centerY + 40 &&
+		mouseX < centerX + 50 &&
+		mouseY < centerY + 50 &&
 		mouseY > centerY
 	) {
 		scr = 1;
-	
 	} else if (
-		mouseX > centerX &&
-		mouseX < centerX + 100 &&
-		mouseY < centerY + 100 &&
-		mouseY > centerY
+		mouseX > src2x &&
+		mouseX < src2x + 50 &&
+		mouseY < src2y + 50 &&
+		mouseY > src2y
+	) {
+		scr = 6;
+	} else if (
+		mouseX > closex &&
+		mouseX < closex + 50  &&
+		mouseY < closey + 50 &&
+		mouseY > closey
 	) {
 		scr = 0;
 	} else if (
-		mouseX > centerX + 210 &&
+		mouseX > centerX &&
 		mouseX < centerX + 100 &&
 		mouseY < centerY + 100 &&
 		mouseY > centerY
 	) {
 		scr = 2;
-	} else if (
-		mouseX > centerX + 70 &&
-		mouseX < centerX + 70 + 40 &&
-		mouseY < centerY + 40 &&
-		mouseY > centerY
-	) {
-		scr = 3;
-	} else if (scr == 0 && mouseX > 330 && mouseX < 365 && mouseY < 270 && mouseY > 220) {
-		scr = 4;
-	}
+	} 
 }
 
 
