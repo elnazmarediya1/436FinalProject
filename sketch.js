36cb1f82dacaf4b5620114cec0238ec5628ed597
elnazmarediya1 <54216751+elnazmarediya1@users.noreply.github.com>
2021-05-02 15:47:01 -0500

    Changes for deployment

--- sketch.js
+++ sketch.js
@@ -2,13 +2,18 @@
 
 function setup() {
 	cnv = createCanvas(3000, 2000);
-	main = loadImage("Xoom.png");
+	main = loadImage("Xoom1.png"); //change to Xoom1 when playing or keep as Xoom
 	ccoin = loadImage("carloscoin.png");
 	wboard = loadImage("WhiteBoardEdit.png");
 	coined = loadImage("coined.png");
 	chat = loadImage("chatedit.png");
 	hearted = loadImage("hearted.png");
 	gray = loadImage("grayheart.png");
+	game = loadImage("startgame.png");
+	q1 = loadImage("q1.png");
+	q2 = loadImage("q2.png");
+	correct = loadImage("correct.png");
+	startscreen = loadImage("startscreen.png");
 	screenRecord = createVideo("screenRecord.mov");
 	screenRecord.hide();
 	cnv.mousePressed(logic);
@@ -23,6 +28,7 @@ function draw() {
 		//rect(790, 735, 35, 30);
 
 		//rect(600, 735, 35, 30);
+		//rect(445, 735, 35, 30);
 	} 
 	else if (scr == 1) {
 		image(ccoin, 10, 10, 1200, 769);
@@ -44,6 +50,25 @@ function draw() {
 	else if (scr == 8) {
 		image(gray, 10, 10, 1200, 769);
 	} 
+	else if (scr == 9) {
+		image(game, 10, 10, 1200, 769);
+		//rect(350, 300, 35, 30);
+	} 
+	else if (scr == 10) {
+		image(startscreen, 10, 10, 1200, 769);
+	} 
+	else if (scr == 11) {
+		image(q1, 10, 10, 1200, 769);
+		//rect(625, 600, 35, 30);
+	} 
+	else if (scr == 12) {
+		image(q2, 10, 10, 1200, 769);
+		//rect(300, 425, 35, 30);
+	} 
+	else if (scr == 13) {
+		image(correct, 10, 10, 1200, 769);
+		//rect(450, 425, 35, 30);
+	} 
 }
 
 function logic() {
@@ -56,6 +81,14 @@ function logic() {
 	chatx = 600;
 	heartx = 1160;
 	hearty = 50;
+	gamex = 440;
+	sscreenx = 350;
+	sscreeny = 300;
+	binaryx = 625;
+	binaryy = 600;
+	q2x = 450;
+	q2y = 425;
+	bux = 300;
 	if (
 		mouseX > centerX &&
 		mouseX < centerX + 50 &&
@@ -94,7 +127,7 @@ function logic() {
 		mouseY < centerY + 50 &&
 		mouseY > centerY
 	) {
-		scr = 3; //change to scr = 8 for carlos' view
+		scr = 3; //change to scr = 8 for carlos' view and 3 for no likes
 	} 
 	else if (
 		mouseX > heartx &&
@@ -104,7 +137,47 @@ function logic() {
 	) {
 		scr = 7;
 	} 
+	else if (
+		mouseX > gamex &&
+		mouseX < gamex + 50 &&
+		mouseY < centerY + 50 &&
+		mouseY > centerY
+	) {
+		scr = 11;   //9 is for prof/host and 11 is for players
+	} 
+	else if (
+		mouseX > sscreenx &&
+		mouseX < sscreenx + 50 &&
+		mouseY < sscreeny + 50 &&
+		mouseY > sscreeny
+	) {
+		scr = 10;
+	} 
+	else if (
+		mouseX > binaryx &&
+		mouseX < binaryx+ 50 &&
+		mouseY < binaryy + 50 &&
+		mouseY > binaryy
+	) {
+		scr = 13;
+	} 
 
+	else if (
+		mouseX > q2x &&
+		mouseX < q2x + 50 &&
+		mouseY < q2y + 50 &&
+		mouseY > q2y
+	) {
+		scr = 12;
+	} 
+	else if (
+		mouseX > bux &&
+		mouseX < bux + 50 &&
+		mouseY < q2y + 50 &&
+		mouseY > q2y
+	) {
+		scr = 13;
+	} 
 }
 
 
